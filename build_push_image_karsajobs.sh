aff1f7979881b115401eac80cb3bd44f2ed71e6e
Zian <ziancrlswong@gmail.com>
2023-11-30 19:03:17 +0700

    commit

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Perintah untuk membuat Docker image dari Dockerfile yang tadi dibuat, dengan nama image item-app, dan memiliki tag v1.
+docker build -t ghcr.io/ziancarlos/karsajobs:latest -f Dockerfile .
+# Login ke Docker Hub (atau GitHub Packages bila menerapkan saran keempat) via Terminal.
+echo $CR_PAT | docker login ghcr.io -u ziancarlos --password-stdin ghcr.io
+# Mengunggah image ke Docker Hub (atau GitHub Packages bila menerapkan saran keempat).
+docker push ghcr.io/ziancarlos/karsajobs:lates
